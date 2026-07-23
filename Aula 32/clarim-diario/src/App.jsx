05f15caf5993172ba40744b14438443e9598ceab
Silvia Abe <silviaabebuzatto@gmail.com>
2026-07-23 08:19:38 -0300

    feat: continuação do projeto Clarim Diário (newscard)

--- Aula 32/clarim-diario/src/App.jsx
+++ Aula 32/clarim-diario/src/App.jsx
@@ -1,9 +1,33 @@
 import Header from "./components/Header/Header"
+import NewsCard from "./components/NewsCard/NewsCard"
+import { noticias } from "./data/Noticias"
+import "./App.css"
 
 function App() {
+  const [ manchete, ...demais ] = noticias
   return (
     <>
       <Header/>
+
+      <main className="container">
+        <section className="manchete">
+          <NewsCard
+            categoria = {manchete.categoria}
+            titulo={manchete.titulo}
+            resumo={manchete.resumo}
+          />
+        </section>
+        <section className="grade">
+          {demais.map((noticia) => (
+            <NewsCard
+              key={noticia.id}
+              categoria={noticia.categoria}
+              titulo={noticia.titulo}
+              resumo={noticia.resumo}
+            />
+          ))}
+        </section>
+      </main>
     </>
   )
 }
---
+++ Aula 32/clarim-diario/src/data/noticias.js
@@ -0,0 +1,26 @@
+export const noticias = [
+  {
+    id: 1,
+    categoria: "Ameaças Urbanas",
+    titulo: "Homem-Aranha destrói viaduto e chama de 'resgate'",
+    resumo: "Prejuízo estimado em milhões. Prefeitura estuda enviar a conta ao vigilante mascarado."
+  },
+  {
+    id: 2,
+    categoria: "Cidade",
+    titulo: "Metrô da linha vermelha tem horário estendido no verão",
+    resumo: "Medida atende antigo pedido dos moradores do Queens."
+  },
+  {
+    id: 3,
+    categoria: "Opinião do Editor",
+    titulo: "Máscaras escondem rostos e intenções",
+    resumo: "Se ele fosse herói de verdade, mostraria a cara. Simples assim."
+  },
+  {
+    id: 4,
+    categoria: "Esportes",
+    titulo: "Eddie Brock lesionado",
+    resumo: "Eddie Brock, A.K.A. Venon (apelido do atleta) está se tratando com Dr. Connors."
+  }
+]
